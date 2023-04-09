ee69b392a833db7e1bc883963762636d57c2c430
khushalkhan <khushalkhanpaktiawal321@gmail.com>
2023-04-09 15:13:43 +0300

    first commit

---
+++ src/LoginInPage.java
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class LoginInPage {
+    public static void main(String[] args) {
+        String userName = "Noor";
+        String password = "pass123";
+
+        System.out.println("userName is "+userName+" password is " + password );
+    }
+}
