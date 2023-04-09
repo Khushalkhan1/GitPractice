4d308596acac5d01136566c64e1bccc6090ba37f
khushalkhan <khushalkhanpaktiawal321@gmail.com>
2023-04-09 17:19:45 +0300

    Login Page Code

---
+++ src/LogInPage1.java
@@ -0,0 +1,10 @@
+public class LogInPage1 {
+    public static void main(String [] args){
+
+        String userName = "karim";
+        String Password = "pass31";
+
+        System.out.println("userName is "+userName+" password is " + Password );
+
+    }
+}
